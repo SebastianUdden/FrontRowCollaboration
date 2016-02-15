f486450285a59c6f82e3bb1237ef047bdeb68a1f
frlindqv <frlindqv@kth.se>
2016-02-15 16:09:58 +0100

    Added js functions

--- FrontRowCollaboration/main.js
+++ FrontRowCollaboration/main.js
@@ -2,3 +2,20 @@
 {
     document.getElementById("test").innerHTML = "test"; 
 }
+
+var x = 1500;
+
+x = y + x;
+
+
+function Alerts(value) {
+
+
+    for (i = 0; i < 10; i++) {
+        alert(value);
+
+    }
+
+}
+
+Alerts(y);
